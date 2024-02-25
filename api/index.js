814a98ac659d6c473f74f73d31fbf0a5212439e6
Jiawei Wu <robertwu1996@gmail.com>
2024-02-25 18:35:48 -0500

    run server and connect DB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB is connected");
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => console.log(error));
