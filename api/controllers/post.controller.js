00b15f81f26a6d4431ec0c567f2e8c6fb5c6222c
Jiawei Wu <robertwu1996@gmail.com>
2024-02-28 00:44:50 -0500

    create get posts api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -28,3 +28,53 @@ export const createPost = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getPosts = async (req, res, next) => {
+  try {
+    // const startIndex, limit, sortby will limit the posts that shows on the page, user can press show more button to see more posts.
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortBy = req.query.order === "asc" ? 1 : -1;
+
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          {
+            title: { $regex: req.query.searchTerm, $options: "i" },
+          },
+          {
+            content: { $regex: req.query.searchTerm, $options: "i" },
+          },
+        ],
+      }),
+    })
+      .sort({ updateAt: sortBy })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalPosts = await Post.countDocuments();
+
+    // get the total posts in the last month
+    const now = new Date();
+    const onMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    const totalPostsInLastMonth = await Post.countDocuments({
+      createdAt: { $gte: onMonthAgo },
+    });
+
+    res.status(200).json({
+      posts,
+      totalPosts,
+      totalPostsInLastMonth,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
