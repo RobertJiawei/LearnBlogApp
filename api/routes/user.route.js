55d85ceed5be053107cf2d9dfcfe5991f2232525
Jiawei Wu <robertwu1996@gmail.com>
2024-02-28 16:29:08 -0500

    create get users api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   deleteUser,
+  getUsers,
   signOut,
   updateUser,
 } from "../controllers/user.controller.js";
@@ -11,5 +12,6 @@ const router = express.Router();
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signOut);
+router.get("/get-users", verifyToken, getUsers);
 
 export default router;
